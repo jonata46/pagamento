e2e7fcd77832b343a4cbdedd97a3eb5411283654
jonata49 <jonatarodrigo531@gmail.com>
2026-09-25 14:24:40 -0300

    Feat: Formas de Pagamento

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ void main() {
     FormaDePagamento pix =  new PagamentoPix();
     pix.processarPagamento();
     pix.processarPagamento();
-
+//vhguyhuoip~´lçkjhgfv
     FormaDePagamento Boleto = new Boleto();
     Boleto.processarPagamento();
     Boleto.processarPagamento();
